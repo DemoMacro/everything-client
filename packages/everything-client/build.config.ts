578d884dc2ebda43b071c9282df9ff7aa68dd4c6
Demo Macro <abc@imst.xyz>
2025-04-03 21:48:04 +0800

    chore: update everything-client to version 0.0.5-edge.0 and enhance build configuration
    - Bump everything-client version to 0.0.5-edge.0.
    - Add new entries for cli-adapter, http-adapter, and ipc-adapter in the build configuration.
    - Update pnpm workspace to include koffi as a built dependency.

--- packages/everything-client/build.config.ts
+++ packages/everything-client/build.config.ts
@@ -2,7 +2,23 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   declaration: true,
-  entries: ["src/index"],
+  entries: [
+    {
+      input: "src/index",
+    },
+    {
+      input: "src/adapters/cli-adapter.ts",
+      name: "cli-adapter",
+    },
+    {
+      input: "src/adapters/http-adapter.ts",
+      name: "http-adapter",
+    },
+    {
+      input: "src/adapters/ipc-adapter.ts",
+      name: "ipc-adapter",
+    },
+  ],
   rollup: {
     inlineDependencies: true,
     emitCJS: true,
